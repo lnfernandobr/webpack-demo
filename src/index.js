3e9edb666ecd4a2f469cf1b29a6eb17d59ce96c4
Fernando <lnfernandobr@gmail.com>
2021-07-28 17:29:36 -0400

    * Added plugins to manager html files  * Added modules to load images, fonts and css files  * Changed the entry and output point

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import _ from "lodash";
+import "./style.css";
+import Cookie from "./bolo.jpeg";
+import printMe from "./print";
 
 function component() {
   const element = document.createElement("div");
 
+  const btn = document.createElement("button");
+
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello 2", "webpack"], " ");
+  element.classList.add("hello");
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  const img = new Image();
+  img.src = Cookie;
+  element.appendChild(img);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|sv|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
